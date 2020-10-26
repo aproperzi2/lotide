5e86b56e642295d45e6c23177ef57633a13bb7aa
Luca Properzi <abramoproperzi@gmail.com>
2020-10-26 22:50:43 +0000

    console.assert

---
+++ lotide/assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual('Lighthouse', 'Bootcamp');
+assertEqual(1, 1);
+
+assertEqual('Calgary', 'Edmonton');
+assertEqual(10, 10);
