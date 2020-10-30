26607d889d6fe3352f771d9fa440556169b93ee0
Luca Properzi <abramoproperzi@gmail.com>
2020-10-30 02:00:36 +0000

    Implement findKey

---
+++ lotide/findKey.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(obj, callback) {
+  for (let item in obj) {
+    callback(obj[item]);
+    if (callback(obj[item]) === true) {
+      return item;
+    }
+  }
+};
+
+const object = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
+
+
+assertEqual(findKey(object, x => x.stars === 2), 'noma'); // => "noma")
