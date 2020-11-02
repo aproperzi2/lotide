c21a30f0673151c22cf9c9d15389f5d3dfeb84cc
Luca Properzi <abramoproperzi@gmail.com>
2020-11-02 15:06:55 -0700

    implement mocha and chai

--- lotide/test/middleTest.js
+++ lotide/test/middleTest.js
@@ -8,13 +8,13 @@ describe("#tail", () => {
   it('should return empty array', () => {
     assert.deepEqual(middle([1, 2]), []);
   })
-  it('should return empty array', () => {
+  it('should return [2, 3]', () => {
     assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
   })
-  it('should return empty array', () => {
+  it('should return [3]', () => {
     assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
   })
-  it('should return empty array', () => {
+  it('should return [3, 4]', () => {
     assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
   })
 
