dc051d289ec938f7b654a6240512cb95b29e4908
Luca Properzi <abramoproperzi@gmail.com>
2020-11-02 14:15:43 -0700

    implement module.exports

---
+++ lotide/eqArrays.js
@@ -0,0 +1,7 @@
+const assertEqual = require('./assertEqual');
+
+const eqArrays = function(arr1, arr2) {
+  return Array.isArray(arr1) && Array.isArray(arr2) && arr1.length === arr2.length && arr1.every((val, index) => val === arr2[index]);
+};
+
+module.exports = eqArrays;
