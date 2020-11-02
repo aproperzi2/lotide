64d942798072978e596c9615ade43037f4d959d5
Luca Properzi <abramoproperzi@gmail.com>
2020-11-02 15:03:52 -0700

    implement mocha and chai

--- lotide/test/middleTest.js
+++ lotide/test/middleTest.js
@@ -1,8 +1,21 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-console.log(assertArraysEqual(middle([1]), [])); // => []
-console.log(assertArraysEqual(middle([1, 2]), [])); // => []
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])); // => [2, 3]
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])); // => [3]
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])); // => [3, 4]
+describe("#tail", () => {
+  it('should return empty array', () => {
+    assert.deepEqual(middle([1]), []);
+  })
+  it('should return empty array', () => {
+    assert.deepEqual(middle([1, 2]), []);
+  })
+  it('should return empty array', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  })
+  it('should return empty array', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  })
+  it('should return empty array', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  })
+
+})
