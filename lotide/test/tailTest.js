a6888c7e98d1257a9d35438b3e6a9462bf2dd1c4
Luca Properzi <abramoproperzi@gmail.com>
2020-11-02 15:04:18 -0700

    implement mocha and chai

--- lotide/test/tailTest.js
+++ lotide/test/tailTest.js
@@ -1,7 +1,10 @@
-const assertEqual = require('../assertEqual');
 const tail = require('../tail');
-
-// Test Case: Check the original array
+const assert = require('chai').assert;
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+
+describe("#tail", () => {
+  it('should return 2', () => {
+    assert.deepEqual(tail(words), 2);
+  })
+
+})
